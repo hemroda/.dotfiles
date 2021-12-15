4416d13f712a1348668851f60e3e28767d3e1d67
hemroda <hemroda@gmail.com>
2021-12-15 22:32:56 +0100

    more plugins ¯\_(ツ)_/¯

--- config/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,30 +1,44 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    highlight = {
-      enable = true, -- false will disable the whole extension
-    },
-    indent = {
-      enable = true,
-    }
+  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  highlight = {
+    enable = true, -- false will disable the whole extension
+  },
+  indent = {
+    enable = true,
+  },
+  autotag = {
+    enable = true,
+    filetypes = { 'html', 'javascript', 'javascriptreact', 'typescriptreact', 'erb', 'vue' },
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
+  },
+  autopairs = {
+    enable = true
   }
+}
   
-  -- Treesitter
-  -- This folding setting will respect your foldminlines and foldnestmax settings.
-  vim.opt.foldmethod = "expr"
-  vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-  local lsp_installer = require("nvim-lsp-installer")
-  lsp_installer.on_server_ready(function(server)
-    local opts = {}
-    if server.name == "sumneko_lua" then
-      opts = {
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { 'vim', 'use' }
-            },
-          }
+-- Treesitter
+-- This folding setting will respect your foldminlines and foldnestmax settings.
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+local lsp_installer = require("nvim-lsp-installer")
+lsp_installer.on_server_ready(function(server)
+  local opts = {}
+  if server.name == "sumneko_lua" then
+    opts = {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim', 'use' }
+          },
         }
       }
-    end
-    server:setup(opts)
-  end)
+    }
+  end
+  server:setup(opts)
+end)
