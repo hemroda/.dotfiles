9125b26aa1d419a0725d3561d5446aecfc77d9d2
hemroda <hemroda@gmail.com>
2022-01-11 17:17:43 +0100

    update install script: source brew at the right moment

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -1,26 +1,26 @@
 
-require('global-settings')
-require('keymaps')
+require("global-settings")
+require("keymaps")
 
 -- ------------------------------------------------------
 -- Plugins management via Packer
 -- ------------------------------------------------------
 require("plugins")
-require('plugins.nvim-tree')
-require('plugins.telescope')
-require('plugins.nvim-treesitter')
-require('plugins.nvim-cmp')
-require('plugins.startify')
-require('plugins.lsp-config')
-require('plugins.floaterm')
-require('plugins.autopairs')
-require('plugins.gitsigns')
-require('plugins.indentline')
-require('nvim-web-devicons').get_icons()
-require('nvim_comment').setup()
-require('lualine').setup()
-require('nvim-ts-autotag').setup({
-  filetypes = { 'html', 'javascript', 'javascriptreact', 'typescriptreact', 'erb', 'vue' },
+require("plugins.nvim-tree")
+require("plugins.telescope")
+require("plugins.nvim-treesitter")
+require("plugins.nvim-cmp")
+require("plugins.startify")
+require("plugins.lsp-config")
+require("plugins.floaterm")
+require("plugins.autopairs")
+require("plugins.gitsigns")
+require("plugins.indentline")
+require("nvim-web-devicons").get_icons()
+require("nvim_comment").setup()
+require("lualine").setup()
+require("nvim-ts-autotag").setup({
+  filetypes = { "html", "javascript", "javascriptreact", "typescriptreact", "erb", "vue" },
 })
 
 vim.cmd [[colorscheme codedark]]
--- config/.config/nvim/lua/plugins.lua
+++ config/.config/nvim/lua/plugins.lua
@@ -1,4 +1,4 @@
-return require('packer').startup(function()
+return require("packer").startup(function()
     -- Packer can manage itself
     use "wbthomason/packer.nvim"
 
@@ -6,37 +6,37 @@ return require('packer').startup(function()
     use "nvim-treesitter/nvim-treesitter"
     -- Themes
     use "tomasiser/vim-code-dark"
-    use 'tomasr/molokai'
+    use "tomasr/molokai"
     use "kyazdani42/nvim-web-devicons"
     use {"nvim-lualine/lualine.nvim", requires = {"kyazdani42/nvim-web-devicons", opt = true}}
-    use {'romgrk/barbar.nvim', requires = {'kyazdani42/nvim-web-devicons'}}
+    use {"romgrk/barbar.nvim", requires = {"kyazdani42/nvim-web-devicons"}}
     -- For indent line
-    use 'lukas-reineke/indent-blankline.nvim'
+    use "lukas-reineke/indent-blankline.nvim"
     -- Searching files and in them easily
     use {"nvim-telescope/telescope.nvim", requires = {"nvim-lua/plenary.nvim"}}
     -- File explorer
-    use {'kyazdani42/nvim-tree.lua', requires = {'kyazdani42/nvim-web-devicons',}}
+    use {"kyazdani42/nvim-tree.lua", requires = {"kyazdani42/nvim-web-devicons",}}
     -- Tools
     use "terrortylor/nvim-comment"
     use {"goolord/alpha-nvim", requires = {"kyazdani42/nvim-web-devicons"}}
-    use 'voldikss/vim-floaterm'
+    use "voldikss/vim-floaterm"
     -- Provide git info
-    use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'}}
-    use {'p00f/nvim-ts-rainbow', after = "nvim-treesitter"}
-    -- use {'windwp/nvim-ts-autotag', event = "InsertEnter", after = "nvim-treesitter"}
-    use 'windwp/nvim-ts-autotag'
-    -- Auto pairs '', "", [], {}, (), ``
-    use 'windwp/nvim-autopairs'
+    use {"lewis6991/gitsigns.nvim", requires = {"nvim-lua/plenary.nvim"}}
+    use {"p00f/nvim-ts-rainbow", after = "nvim-treesitter"}
+    -- use {"windwp/nvim-ts-autotag", event = "InsertEnter", after = "nvim-treesitter"}
+    use "windwp/nvim-ts-autotag"
+    -- Auto pairs "", "", [], {}, (), ``
+    use "windwp/nvim-autopairs"
     -- Collection of configurations for built-in LSP client
     use "neovim/nvim-lspconfig"
     use "williamboman/nvim-lsp-installer"
     -- Autocompletion plugin
-    use 'hrsh7th/cmp-nvim-lsp'
+    use "hrsh7th/cmp-nvim-lsp"
     -- For buffer completion
-    use 'hrsh7th/cmp-buffer'
+    use "hrsh7th/cmp-buffer"
     -- For path completion
-    use 'hrsh7th/cmp-path'
+    use "hrsh7th/cmp-path"
     -- Autocompletion plugin
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-cmdline'
+    use "hrsh7th/nvim-cmp"
+    use "hrsh7th/cmp-cmdline"
 end)
